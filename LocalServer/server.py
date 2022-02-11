56ae93970f9b03f3e73055e3764dad9c61ff9ee9
chrisrosler <70918483+chrisrosler@users.noreply.github.com>
2022-02-11 13:42:21 -0700

    ZFR Parser
    Just an extremely simple framework that runs.

--- LocalServer/server.py
+++ LocalServer/server.py
@@ -1,5 +1,7 @@
 from flask import Flask
 from flask import render_template
+import fileParser
+
 
 app = Flask(__name__)
 
@@ -9,6 +11,6 @@ app = Flask(__name__)
 def index():
     return render_template('index.html', title="Dissertation Validator")
 
-
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=81)
+
