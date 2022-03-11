99eeb0eca98792a8c92759cb83232f308a9dd09c
Cam Alberghini <fanfare22@gmail.com>
2022-03-11 13:39:37 -0700

    fileUpload - Includes example html

---
+++ LocalServer/parserTEMP-updatelater/fileUpload.py
@@ -0,0 +1,32 @@
+# Simple HTML for This File Uploader
+# <html>
+#   <body>
+#       <form action = "http://localhost:5000/uploader" method = "POST" enctype = "multipart/form-data">
+#           <input type = "file" name = "file" />
+#           <input type = "submit"/>
+#       </form>
+#   </body>
+# </html>
+
+from flask import Flask, render_template, request
+import werkzeug.utils
+from varname import nameof
+import os
+app = Flask(__name__)
+
+
+path = "/Uploads"
+
+
+@app.route('/upload')
+def upload_file():
+    return render_template('upload.html')
+
+
+@app.route('/uploader', methods=['GET', 'POST'])
+def upload_file(zFile):
+    if request.method == 'POST':
+        fName = nameof(zFile)
+        f = request.files[fName]
+        f.save(os.path.join(path, werkzeug.secure_filename(f.filename)))
+        return 'file uploaded successfully'
