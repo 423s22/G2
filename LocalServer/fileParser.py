c563ba28cdc0a12225f41807cedf36893e330e7c
chrisrosler <70918483+chrisrosler@users.noreply.github.com>
2022-04-15 12:51:02 -0600

    TypoChecking

--- LocalServer/fileParser.py
+++ LocalServer/fileParser.py
@@ -1,3 +1,4 @@
+import copy
 from pathlib import Path
 import zipfile
 import xml.etree.ElementTree
@@ -21,8 +22,12 @@ class validatorMain:
                     newParagraph.caps = True
                 elif str(var.tag) == "{http://schemas.openxmlformats.org/wordprocessingml/2006/main}t":
                     newParagraph.text += var.text
+                elif str(var.tag) == "{http://schemas.openxmlformats.org/wordprocessingml/2006/main}proofErr":
+                    newParagraph.proofErr = True
                 else:
-                    pass#print(var.tag)
+                    #pass#
+                    print(var.tag)
+                    #print(var.attrib)
             else:#Used to instantiate your first paragraph
                 if str(var.tag) == "{http://schemas.openxmlformats.org/wordprocessingml/2006/main}p":
                     newParagraph.paraId = var.attrib["{http://schemas.microsoft.com/office/word/2010/wordml}textId"]
@@ -37,6 +42,11 @@ class validatorMain:
         fp = open('Changes/requiredChanges.txt', 'w')
         empty = True
         byLine = 0
+        lineVar = 0
+        #for i in document:
+        #    print(i.text)
+        #    if i.text == "":
+        #        print(i.paraId)
         try:
             for i in range(4): #Ensure the by line exists, and then that the preceeding lines are capitalized. Maximum length = 4 lines.
                 if document[i].text == "by":
@@ -63,31 +73,48 @@ class validatorMain:
 
             if not document[byLine+6].text == "in":
                 empty = False
-                print(document[byLine+6].text)
                 fp.write('Missing \"in\" line prior to program name\n')
             if not document[byLine+8].text == "MONTANA STATE UNIVERSITY" :
                 empty = False
                 fp.write('Missing university, or university not formatted correctly (All Capitalized)\n')
-            if not checkDateFormat(document[byLine + 9].text):
+            if not checkDateFormat(document[byLine + 10].text):
                 empty = False
-                print(document[byLine+10].text)
                 fp.write('Date is formatted as month written out and first letter capitalized, then year\n')
+            lineVar = byLine
+            while document[byLine + 11].text == "":
+                byLine +=1
+            print(document[byLine + 11].text)
             if not document[byLine+11].text.__contains__("COPYRIGHT"):
                 empty = False
-                fp.write('Missing copyright')
+                print(document[byLine+11].text)
+                fp.write('Missing copyright\n')
             if not document[byLine+12].text.__contains__("by"):
                 empty = False
                 fp.write('Missing by after copyright\n')
-            if not document[byLine+13].text == document[byLine+1]:
+            if not document[byLine+13].text == document[lineVar+1].text:
                 empty = False
+                print(document[byLine+13].text)
+                print(document[lineVar+1].text)
                 fp.write('Missing name consistency\n')
-            if not document[byLine+14].text == "Year":
+            if not document[byLine+14].text == "2022" and not document[byLine+14].text == "2023":
                 empty = False
                 fp.write('Missing year\n')
             if not document[byLine+15].text == "All Rights Reserved":
                 empty = False
                 fp.write('Missing All Rights Reserved\n')
-
+            while document[byLine+16].text != "CHAPTER ONE":
+                byLine +=1
+            endLine = byLine + 16
+            while document[endLine].text != "References":
+                endLine +=1
+            body = copy.deepcopy(document[byLine:endLine])
+            print(byLine)
+            print(endLine)
+            print(len(body))
+            for j in range(len(body)):
+                if body[j].proofErr:
+                    empty = False
+                    fp.write('Please check paragraph ' + str(j+byLine) + ' for a typo\n')
             if empty:
                 fp.write("All good")
                 fp.close
